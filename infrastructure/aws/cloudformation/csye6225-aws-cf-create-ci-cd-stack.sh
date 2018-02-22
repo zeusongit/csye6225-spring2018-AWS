6638490bccb17f6b13150d83829affa1f9dc3a20
Ashish <aggarwal.ash@husky.neu.edu>
2018-02-22 15:31:14 -0500

    A6 update

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-ci-cd-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-ci-cd-stack.sh
@@ -2,9 +2,12 @@
 #Variables
 
 stackname=$1
+#domain=$(aws route53 list-hosted-zones --query HostedZones[0].Name --output text)
+#s3domain="web-app.$s3domain"
+s3domain="code-deploy.csye6225-spring2018-aggarwalash.me"
+echo "S3 Domain Name: $s3domain"
 
-
-createOutput=$(aws cloudformation create-stack --stack-name $stackname --capabilities CAPABILITY_NAMED_IAM --template-body file://csye6225-cf-ci-cd.json)
+createOutput=$(aws cloudformation create-stack --stack-name $stackname --capabilities CAPABILITY_NAMED_IAM --template-body file://csye6225-cf-ci-cd.json--parameters ParameterKey=s3domain,ParameterValue=$s3domain)
 
 
 if [ $? -eq 0 ]; then
