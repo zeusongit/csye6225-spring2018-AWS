d47e224a056f458ee8747933bc300e6b0a9a8445
singhvika <35575199+singhvika@users.noreply.github.com>
2018-02-24 15:38:53 -0500

    Update afterInstall.sh

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -3,3 +3,6 @@
 # update the permission and ownership of WAR file in the tomcat webapps directory
 echo "#CSYE6225: doing after install"
 cd /var/webapp/csye6225-spring2018
+sudo unzip csye6225-spring2018.zip -d ./
+cp csye6225-spring/* ./
+rm -rf csye6225-spring2018.zip csye6225-spring2018
