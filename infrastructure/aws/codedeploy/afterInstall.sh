71178c7891e02b43231b2fc0652f26784a9c7585
singhvika <35575199+singhvika@users.noreply.github.com>
2018-02-24 15:56:18 -0500

    Update afterInstall.sh

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -4,12 +4,12 @@
 echo "#CSYE6225: doing after install"
 cd /var/webapp/csye6225-spring2018
 sudo unzip csye6225-spring2018.zip -d ./
-echo pwd
-pwd
-echo "now copying"
-sudo cp -a csye6225-spring/. ./
-chmod -R 777 csye6225-spring2018
-sudo rm -rf csye6225-spring2018.zip csye6225-spring2018
-echo "pwd and final files"
-pwd
-ls -lrt
+#echo pwd
+#pwd
+#echo "now copying"
+#sudo cp -a csye6225-spring/. ./
+#chmod -R 777 csye6225-spring2018
+#sudo rm -rf csye6225-spring2018.zip csye6225-spring2018
+#echo "pwd and final files"
+#pwd
+#ls -lrt
