1ce93fe6de3777992cc44bd5b04b789e765eb01f
Ashish <aggarwal.ash@husky.neu.edu>
2018-02-24 00:42:46 -0500

    appspec update

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
@@ -19,10 +19,10 @@ sgdb=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=$netst
 echo "DB SG: $sgdb"
 iaminstance="EC2ToS3BucketInstanceProfile"
 echo "Instance Profile Name: $iaminstance"
-#domain=$(aws route53 list-hosted-zones --query HostedZones[0].Name --output text)
-#s3domain="web-app.$s3domain"
-s3domain="web-app.csye6225-spring2018-aggarwalash.me"
-echo "S3 Domain Name: $s3domain"
+domain=$(aws route53 list-hosted-zones --query HostedZones[0].Name --output text)
+trimdomain=${domain::-1}
+s3domain="web-app.$trimdomain"
+echo "S3 Domain: $s3domain"
 
 createOutput=$(aws cloudformation create-stack --stack-name $stackname --template-body file://csye6225-cf-application.json --parameters ParameterKey=stackname,ParameterValue=$stackname ParameterKey=dbsubnet,ParameterValue=$dbsubnet ParameterKey=s3domain,ParameterValue=$s3domain ParameterKey=ec2Subnet,ParameterValue=$subnet1 ParameterKey=ec2SecurityGroup,ParameterValue=$sgec2 ParameterKey=dbSecurityGroupId,ParameterValue=$sgdb ParameterKey=iaminstance,ParameterValue=$iaminstance)
 
--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-ci-cd-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-ci-cd-stack.sh
@@ -2,13 +2,17 @@
 #Variables
 
 stackname=$1
-#domain=$(aws route53 list-hosted-zones --query HostedZones[0].Name --output text)
-#s3domain="web-app.$s3domain"
-s3domain="code-deploy.csye6225-spring2018-aggarwalash.me"
-echo "S3 Domain Name: $s3domain"
 
-createOutput=$(aws cloudformation create-stack --stack-name $stackname --capabilities CAPABILITY_NAMED_IAM --template-body file://csye6225-cf-ci-cd.json --parameters ParameterKey=s3domain,ParameterValue=$s3domain)
+domain=$(aws route53 list-hosted-zones --query HostedZones[0].Name --output text)
+trimdomain=${domain::-1}
+s3domain="code-deploy.$trimdomain"
+echo "S3 Domain: $s3domain"
+appname="csye6225CodeDeployApplication"
+echo $appname
+depname="csye6225CodeDeployApplication-depgroup"
+echo $depname
 
+createOutput=$(aws cloudformation create-stack --stack-name $stackname --capabilities CAPABILITY_NAMED_IAM --template-body file://csye6225-cf-ci-cd.json --parameters ParameterKey=s3domain,ParameterValue=$s3domain  ParameterKey=appname,ParameterValue=$appname ParameterKey=depname,ParameterValue=$depname)
 
 if [ $? -eq 0 ]; then
 	echo "Creating stack..."
