f3b21eac3f2c689e15ee8c8fc7e168f954262b4a
Ashish <aggarwal.ash@husky.neu.edu>
2018-02-28 18:15:27 -0500

    final

--- infrastructure/aws/dynamoDB/createDynamoDB.sh
+++
@@ -1,7 +0,0 @@
-#Give csye6225_dydb for $1
-
-# Create CloudFormation Stack
-echo "Creating CloudFormation Stack"
-aws cloudformation create-stack --stack-name $1 --template-body file://./createDynamoDBTemplate.json
-
-aws cloudformation validate-template --template-body file://./createDynamoDBTemplate.json
