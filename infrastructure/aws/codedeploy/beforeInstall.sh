78ddf74f7f18c259ecf20e886b82ab20ccc05284
Sonali Chaudhari <chaudhari.s@husky.neu.edu>
2018-02-28 17:11:36 -0500

    Assignemnt-6

--- infrastructure/aws/codedeploy/beforeInstall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# change to tomcat webapps directory.
-# this directory will be different for different tomcat versions.
-cd /var
-sudo rm -rf csye6225-spring2018.zip
-sudo rm -rf webapp
--- infrastructure/aws/dynamoDB/deleteDynamoDB.sh
+++
@@ -1,4 +0,0 @@
-#Give csye6225dydb for $1
-
-echo "Deleting CloudFormation Stack"
-aws cloudformation delete-stack --stack-name $1
