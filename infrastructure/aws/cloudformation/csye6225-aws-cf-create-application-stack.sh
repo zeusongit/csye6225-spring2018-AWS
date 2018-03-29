9bf70ae83d706b35751116caa6d69d9fc24a6c82
Sonali Chaudhari <chaudhari.s@husky.neu.edu>
2018-03-28 20:26:40 -0400

    App stack sns arn

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
@@ -17,7 +17,21 @@ sgec2=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=$nets
 echo "EC2 SG: $sgec2"
 sgdb=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=$netstack-csye6225-db-secuitygroup" --query SecurityGroups[*].GroupId --output text)
 echo "DB SG: $sgdb"
-
+iaminstance="EC2ToS3BucketInstanceProfile"
+echo "Instance Profile Name: $iaminstance"
+domain=$(aws route53 list-hosted-zones --query HostedZones[0].Name --output text)
+trimdomain=${domain::-1}
+s3domain="web-app.$trimdomain"
+echo "S3 Domain: $s3domain"
+var2=$(aws sns list-topics --query Topics[].TopicArn --output text)
+var1="LambdaTopic"
+for a in ${var2[*]}
+do
+   if [[ "$a" =~ "$var1" ]]; then
+   		topicarn=$a
+	fi
+done
+echo $topicarn
 
 createOutput=$(aws cloudformation create-stack --stack-name $stackname --template-body file://csye6225-cf-application.json --parameters ParameterKey=stackname,ParameterValue=$stackname ParameterKey=dbsubnet,ParameterValue=$dbsubnet ParameterKey=s3domain,ParameterValue=$s3domain ParameterKey=ec2Subnet,ParameterValue=$subnet1 ParameterKey=ec2SecurityGroup,ParameterValue=$sgec2 ParameterKey=dbSecurityGroupId,ParameterValue=$sgdb ParameterKey=iaminstance,ParameterValue=$iaminstance)
 
