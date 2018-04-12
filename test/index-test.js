baab16bc5d1164c463320a803131174be38eb78c
Sonali Chaudhari <chaudhari.s@husky.neu.edu>
2018-04-12 18:24:50 -0400

    reset

--- test/index-test.js
+++
@@ -1,9 +0,0 @@
-var expect = require('chai').expect;
-var alwaysTrueTest = require('../alwaysTrueTest.js');
-describe('alwaysTrue()',function(){
-  it('should always return true',function(){
-    var trueCondition = true;
-    var testForTrue = alwaysTrueTest();
-    expect(testForTrue).to.be.equal(testForTrue);
-  });
-});
